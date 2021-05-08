b17dfbe9cec37ddd84333f72944db997fd176836
xuming624 <xuming624@qq.com>
2021-05-08 19:37:48 +0800

    add parser demo.

---
+++ tests/parser_demo.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+@author:XuMing(xuming624@qq.com)
+@description: 
+"""
+import sys
+
+sys.path.append('..')
+
+from companynameparser import parser
+
+if __name__ == '__main__':
+    m = parser.Parser()
+    with open('../companynameparser/data/company_demo.txt', 'r', encoding='utf-8') as fr, \
+            open('company_names_result.txt', 'w', encoding='utf-8') as fw:
+        count = 0
+        for line in fr:
+            i = line.strip()
+            count += 1
+            r = m.parse(i)
+            b = r['brand']
+            print(i, r['place'], r['brand'], r['trade'], r['suffix'])
+            fw.write(i + '\t' + b + '\n')
